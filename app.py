5a6e4a2ec2d2689e579596458f8f87ad40c285d8
Charvi Bannur <77164319+charvibannur@users.noreply.github.com>
2021-02-28 02:55:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-import os
 
-from flask import Flask, render_template,request
+from flask import Response, send_from_directory
+from flask import Flask, render_template, request, send_file
+from pytesseract import pytesseract
 from werkzeug.utils import secure_filename
+import numpy as np
+from keras.preprocessing import image
+from flask import Flask, request, render_template
+from PIL import Image
+
+
 from gtts import gTTS
 import os
 from langdetect import detect
-
-
 
 app = Flask(__name__)
 
@@ -14,36 +19,58 @@ app = Flask(__name__)
 def upload():
     return render_template('index.html')
 
-@app.route('/generate', methods=['GET', 'POST'])
+@app.route('/predict2', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-          if request.form['btn-text']=='read_text':
-            f = request.files['file']
-            basepath = os.path.dirname(__file__)
-            file_path = os.path.join(
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
-            f.save(file_path)
-          elif request.form['btn-audio']=='read_audio':
-              read_audio()
-          else:
-              pass
-
-
+        f.save(file_path)
+        max_length = 32
+        pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+        extractedInformation = pytesseract.image_to_string(Image.open(file_path),
+                                                           lang="eng+fra+ita+deu+spa+por+nld")
+        Text_generated = extractedInformation
+        return Text_generated
     return None
 
-@app.route('/', methods=['POST'])
-def read_audio():
-    if request.method == "POST":
-        user = request.form["test_temp"]
-        Text_generated="hello"
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload_file2():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        max_length = 32
+        pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+        extractedInformation = pytesseract.image_to_string(Image.open(file_path),
+                                                           lang="eng+fra+ita+deu+spa+por+nld")
+        Text_generated = extractedInformation
         language = detect(Text_generated)
         text = Text_generated
-        speech = gTTS(text = text, lang = language, slow = False)
-        speech.save("text2.mp3")
-        os.system("start text2.mp3")
-def read_text():
-    pass
+        speech = gTTS(text=text, lang=language, slow=False)
+        speech.save("static/text2.mp3")
+        return send_file(
+            "static/text2.mp3",
+            mimetype="audio/mpeg",
+            as_attachment=True,
+            attachment_filename="text2.mp3")
+    return None
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -24,12 +24,38 @@ $(document).ready(function () {
         readURL(this);
     });
 
+    $('#btn-predict2').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+
+        // Show loading animation
+        $('.loader').show();
+
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict2',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('.loader').hide();
+                $('#result').fadeIn(600);
+                $('#result').text(' Result:  ' + data);
+                $('#myaudio').fadeIn(600);
+                $('#myaudio').show();
+                console.log('Success!');
+            },
+        });
+    });
+
     // Predict
     $('#btn-predict').click(function () {
         var form_data = new FormData($('#upload-file')[0]);
 
         // Show loading animation
-        $(this).hide();
         $('.loader').show();
 
         // Make prediction by calling api /predict
@@ -42,10 +68,13 @@ $(document).ready(function () {
             processData: false,
             async: true,
             success: function (data) {
-                // Get and display the result
+
                 $('.loader').hide();
-                $('#result').fadeIn(600);
-                $('#result').text(' Result:  ' + data);
+                document.getElementById('audioresult').innerHTML = '<audio id="audio-player" controls="controls" src="static/text2.mp3" type="audio/mpeg">';
+                $("myaudio").src="static/text2.mp3";
+                $("myaudio").type="mpeg"
+                $("myaudio").load();
+                $('myaudio').play();
                 console.log('Success!');
             },
         });
